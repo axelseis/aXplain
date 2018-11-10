28a1801634a87ab92a9e05f2b6e582329b350db5
unknown <axel@axflash.com>
2018-11-10 12:56:18 +0100

    feat(logger): Add log function as console.log wrapper

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-import "./src/lib/logger.js";
+import { log } from "./src/lib/logger.js";
 import App from "./src/components/App/App.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     const app = new App(document.getElementById("App"));
+    log(`inited ${app.name}`)
 })
--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,15 +1,20 @@
 import { state } from './store.js'
 
-const isDev = window.location.port == 3000;
+const isDev = window.location.port === 3000;
 
 if(isDev){
     document.addEventListener('state', (ev) => {
-        const type = ev.detail.type;
+        const {type,payload} = {...ev.detail};
         if(type.indexOf('nolog') === -1){
-            console.groupCollapsed(type, ev.detail.payload)
+            console.groupCollapsed(type, payload)
             console.log('state:', state)
             console.groupEnd();
         }
     })
 }
 
+export function log(message) {
+    if(isDev){
+        console.log('message', message);
+    }
+}
